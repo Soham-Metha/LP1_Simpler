2899381c63a85a2c9ad4328623bac93d4de0a179
Soham Metha <sohammetha01@gmail.com>
2025-11-05 23:39:38 +0530

    AP2: spacing

--- assembler_pass_2.cpp
+++ assembler_pass_2.cpp
@@ -18,7 +18,7 @@ const string no_mc = "-------";
 
 string inst_handler(string inst)
 {
-    if (inst.substr(1, 2) == "AD")      { return no_mc; } 
+    if      (inst.substr(1, 2) == "AD") { return no_mc; } 
     else if (inst.substr(1, 2) == "IS") { return str_chop(inst, 4, 1); }
     else if (inst.substr(1, 2) == "DL") { return str_chop(inst, 4, 1) == "01" ? "00" : no_mc; }
     else                                { return "UNKNOWN INST '" + inst + "'"; }
@@ -26,7 +26,7 @@ string inst_handler(string inst)
 
 string operand_handler(string opr)
 {
-    if (opr.size() == 3)                { return str_chop(opr, 1, 1); }
+    if      (opr.size() == 3)           { return str_chop(opr, 1, 1); }
     else if (opr[1] == 'C')             { return str_chop(opr, 3, 1); }
     else if (opr[1] == 'S')             { return sym_deref(sym_table, str_chop(opr,3,1)); }
     else if (opr[1] == 'L')             { return sym_deref(lit_table, str_chop(opr,3,1)); }
